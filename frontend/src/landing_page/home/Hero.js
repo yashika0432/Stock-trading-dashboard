95478d2426a1fc2629361f566e0693cfec637972
yashika <yashika0432@gmail.com>
2025-11-05 19:04:19 +0530

    herosection

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -2,15 +2,20 @@ import React, { Component } from "react";
 
 function Hero() {
   return (
-    <div className="container">
-      <div className="row">
-        <img src="media/images/homeHero.png" alt="Hero"></img>
-        <h1>Invest in everything</h1>
+    <div className="container p-5 mb-5">
+      <div className="row text-center">
+        <img src="media/images/homeHero.png" alt="Hero" className="mb-5"></img>
+        <h1 className="mt-5">Invest in everything</h1>
         <p>
           Online platform to invest in stocks, derivatives, mutual funds and
           more
         </p>
-        <button>SignUp Now</button>
+        <button
+          className="p-2 btn btn-primary fs-5"
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          SignUp Now
+        </button>
       </div>
     </div>
   );
